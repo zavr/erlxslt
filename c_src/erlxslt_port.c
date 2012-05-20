261ceb6d4de06fc52b6c1a4e0f50acafe2f69af0
w-495 <w@w-495.ru>
2012-05-20 16:20:21 +0400

    take in transfom part

---
+++ c_src/erlxslt_port.c
@@ -0,0 +1,87 @@
+#include "erlxslt_adapter.h"
+
+/**
+    \fn read_exact
+**/
+int read_exact(unsigned char *buf, int len) {
+    int i, got = 0;
+    do {
+        if ((i=read(0, buf+got, len-got)) <= 0)
+            return i;
+        got += i;
+    } while (got < len);
+    return len;
+}
+
+/**
+    \fn write_exact
+**/
+int write_exact(const unsigned char *buf, int len) {
+    int i, wrote = 0;
+    do {
+        if ((i=write(1, buf+wrote, len-wrote)) <= 0)
+            return i;
+        wrote += i;
+    } while (wrote < len);
+    return len;
+}
+
+/**
+    \fn read_cmd
+**/
+int read_cmd(unsigned char *buf) {
+    if (read_exact(buf, PACKET_SIZE) != PACKET_SIZE)
+        return -1;
+    return read_exact(buf, LEN(buf));
+}
+
+/**
+    \fn read_alloc_cmd
+        as_string == 1 ==> reserva 1 byte mas y devuelve ASCIIZ
+**/
+unsigned char *read_alloc_cmd(int as_string) {
+    unsigned char size[PACKET_SIZE];
+    unsigned char *buf;
+    int len;
+    if (read_exact(size, PACKET_SIZE) != PACKET_SIZE)
+        return NULL;
+    len = LEN(size);
+    buf = (unsigned char *) malloc(len+as_string);
+    if (read_exact(buf, len) == len) {
+        if (as_string) 
+            buf[len] = '\0';
+        return buf;
+    }
+    else {
+        free(buf);
+        return NULL;
+    }
+}
+
+/**
+    \fn write_cmd
+**/
+int write_cmd(const char *buf) {
+    return write_cmd((const unsigned char*)buf, strlen((const char*)buf));
+}
+
+/**
+    \fn write_cmd
+**/
+int write_cmd(const unsigned char *buf, int len) {
+    unsigned char str[PACKET_SIZE];
+    PUT_INT(len, str);
+    if (write_exact(str,PACKET_SIZE) != PACKET_SIZE)
+        return -1;
+    return write_exact(buf, len);
+}
+
+/**
+    \fn write_int
+**/
+void write_int(int x) {
+    unsigned char r[PACKET_SIZE];
+    PUT_INT(x,r);
+    write_cmd(r, PACKET_SIZE);
+}
+
---
+++ c_src/erlxslt_transform.c
@@ -0,0 +1,62 @@
+#include "erlxslt_adapter.h"
+
+
+/**
+    \fn save_to_string
+**/
+xstr_t save_to_string(xdp_t result, xsp_t xsl){
+    int res_size;
+    xc_t *res_buff;
+    /*!
+        xsltSaveResultToString(&res_buff, &res_size, result, xsl);
+            vs
+        xmlDocDumpMemory(result, &res_buff, &res_size);
+        Первый вариант правильнее, т.к. он учитывает
+            параментры вывода xsl:output
+    */
+    xsltSaveResultToString(&res_buff, &res_size, result, xsl);
+    xstr_t xstr = {res_buff,    res_size};
+    return xstr;
+}
+
+/**
+    \fn free
+**/
+void free(xstr_t xstr) {
+    free(xstr);
+}
+
+/**
+    \fn free
+**/
+void free(xc_t *res_buff) {
+    xmlFree(res_buff);
+}
+
+/**
+    \fn free
+**/
+void free(xdp_t doc) {
+    xmlFreeDoc(doc);
+}
+
+/**
+    \fn apply_xsl
+**/
+xdp_t apply_xsl(xsp_t xsl, xdp_t doc) {
+    return xsltApplyStylesheet(xsl, doc, NULL);
+}
+
+/**
+    \fn parse_xml
+**/
+xdp_t parse_xml(char *xml_str) {
+    return xmlParseMemory((const char *)xml_str, strlen(xml_str));
+}
+
+/**
+    \fn parse_xslt
+**/
+xsp_t parse_xslt(const xc_t* xslfile) {
+    return xsltParseStylesheetFile((const xc_t*) xslfile);
+}
